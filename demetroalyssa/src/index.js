66116125721a3ecb27356c10d97c2c365093d895
Alyssa DeMetro <leanndemetro@gmail.com>
2021-08-02 13:52:07 -0400

    come on please

--- demetroalyssa/src/index.js
+++ demetroalyssa/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import reportWebVitals from './reportWebVitals';
 import "./index.css";
 import App from "./App";
 
@@ -9,7 +8,3 @@ import registerServiceWorker from './registerServiceWorker.js';
 ReactDOM.render( <App /> , document.getElementById('root'));
 
 registerServiceWorker();
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
