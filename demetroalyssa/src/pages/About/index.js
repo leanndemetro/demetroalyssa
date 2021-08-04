8ba2d953dbbdacac975bcbc9f612b2b545dd5e0b
Alyssa DeMetro <leanndemetro@gmail.com>
2021-08-03 20:21:59 -0400

    resume link update

--- demetroalyssa/src/pages/About/index.js
+++ demetroalyssa/src/pages/About/index.js
@@ -17,7 +17,7 @@ function About() {
                 </p>
       <p className="aboutTextTwo">A UCF Full-Stack Web Development Certificate holder with a passion for UX/UI design, Alyssa spends her time working as a freelance website design specialist and application developer. Elegance and simplistic modernity are at the forefont of her design technique. For prospective employers, or for further information regarding website design services, please <a className="contactLink" href="/Contact">contact</a> her directly.</p>
                <br></br> 
-               <p className="resumeText"> <a className="resumeLink" target="__blank" href="https://www.linkedin.com/in/alyssa-de-metro-59abba1b0/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAADFMxcYB0XJ1lHN-GVVtohMQbcYjdf0W4pc,1635465091863)/">RESUME  </a>/ <a className="resumeLink" href="/Portfolio"> PORTFOLIO</a></p>
+               <p className="resumeText"> <a className="resumeLink" target="__blank" href="https://www.linkedin.com/in/alyssa-de-metro-59abba1b0/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAADFMxcYB0XJ1lHN-GVVtohMQbcYjdf0W4pc,1635465288229)/">RESUME  </a>/ <a className="resumeLink" href="/Portfolio"> PORTFOLIO</a></p>
                 
                 </div>
 
