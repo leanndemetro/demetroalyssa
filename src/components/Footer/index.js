24f424f80baf51cbf8d28db2533b55530c8795db
Alyssa DeMetro <leanndemetro@gmail.com>
2021-08-09 15:58:37 -0400

    social icons added to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import "./style.css"
-
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 
 class Footer extends Component {
     render() {
         return (
-            <span className="footer">
+            <div className="footer">
             <hr></hr>
+            <a className="socialIcons" href="https://www.linkedin.com/in/alyssa-de-metro-59abba1b0/"><FaLinkedin /></a>
+          
+            <a className="socialIcons" href="https://github.com/leanndemetro"><FaGithub /></a>
+           
             <br></br>
-          </span>
+          </div>
         );
     }
 }
